60693ea87967d809ce09ae130de8a930a80dc944
Aniket Kumar <aniket.kumar@beehyv.com>
2025-04-29 12:16:27 +0530

    update padding

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -23,7 +23,7 @@ function Home() {
   ];
 
   return (
-    <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-16">
+    <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 ">
       <div className="flex flex-col md:flex-row items-center gap-8 md:gap-12 min-h-screen">
         {/* Image Section */}
         <motion.div
